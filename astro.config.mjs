7cd523ce31066b6973975c88ec3055b4676596e8
liufulin <976529466@qq.com>
2024-07-21 16:42:04 +0900

    feat: te form and symbol

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,27 +1,31 @@
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
 console.log(import.meta.env.BASE_URL);
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://lfl976.github.io',
-	base: '/learn-japanese',
+	site: "https://lfl976.github.io",
+	base: "/learn-japanese",
 	server: {
 		host: true,
 	},
 	integrations: [
 		starlight({
-			title: '日语学习网站',
+			title: "日语学习网站",
 			social: {
-				github: 'https://github.com/withastro/starlight',
+				github: "https://github.com/withastro/starlight",
 			},
 			sidebar: [
 				{
-					label: 'Guides',
-					autogenerate: { directory: 'guides' },
+					label: "Guides",
+					autogenerate: { directory: "guides" },
 				},
 				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
+					label: "Conjugation",
+					autogenerate: { directory: "conjugation" },
+				},
+				{
+					label: "Reference",
+					autogenerate: { directory: "reference" },
 				},
 			],
 		}),
